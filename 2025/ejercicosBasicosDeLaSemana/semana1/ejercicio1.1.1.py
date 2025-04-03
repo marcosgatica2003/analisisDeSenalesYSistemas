6f435265ae6d273ac87e1c216502493e114f19ea
marcosgatica2003 <marcosgatica1112@gmail.com>
2025-04-03 10:16:16 -0300

    Reorganizado de ejerccicios semana1

--- 2025/ejercicosBasicosDeLaSemana/semana1/ejercicio1.1.1.py
+++
@@ -1,8 +0,0 @@
-import cmath
-
-
-def evaluar(z, exponente):
-    return z**exponente
-print(evaluar(0 + 1j, 8))
-print(evaluar(0 + 1j, 105))
-
--- 2025/ejercicosBasicosDeLaSemana/semana1/ejercicio1.1.2d.py
+++
@@ -1,7 +0,0 @@
-z1 = 5 - 4j
-z2 = 3 + 7j
-z3 = 4 + 2j
-z4 = 2 - 3j
-
-result = (z1-z2)/(z2+z3)
-print(f"{result:.2f}")
